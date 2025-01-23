eca0666e0951e2583025d86f2aec7f35e508fe17
abhishekchaurasiya <abhichek2580@gmail.com>
2025-01-23 22:25:09 +0530

    Add app.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import CustomerData from "./components/CustomerData";
+
+import Customer from "./components/Customer";
+
+const App = () => {
+  return (
+    <div>
+      {/* <CustomerData /> */}
+      <Customer />
+    </div>
+  );
+};
+
+export default App;
